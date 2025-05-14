370fcf4f56840da4efc0e555eb13b8740d202f45
Supardi G <pardiwan267@gmail.com>
2025-05-14 10:21:04 +0700

    fix

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import userController from "./controllers/user";
-
-const app = express();
-
-app.use(express.json());
-app.use("/users", userController);
-
-export default app;
